274086c5d7e979a0bd620270a00fe909a6a31cff
Tracylu2015 <89538089+Tracylu2015@users.noreply.github.com>
2021-10-17 14:28:17 -0700

    Change defalt db connection

--- miniFig_app/config/config.py
+++ miniFig_app/config/config.py
@@ -4,5 +4,5 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config(object):
     # ...
     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL') or \
-        'sqlite:///' + os.path.join(basedir, 'app.db')
+        'mysql+pymysql://root:rootroot@localhost:3306/minifigs'
     SQLALCHEMY_TRACK_MODIFICATIONS = True
