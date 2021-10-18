e7f19c92b3453048af8443b2b0e510bae5ed6f47
Tracylu2015 <89538089+Tracylu2015@users.noreply.github.com>
2021-10-17 17:05:45 -0700

    Change default to localhost:3306

--- miniFig_app/config/config.py
+++ miniFig_app/config/config.py
@@ -4,6 +4,6 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config(object):
     # ...
     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL') or \
-        'sqlite:///' + os.path.join(basedir, 'app.db')
+        'mysql+pymysql://root:rootroot@localhost:3306/minifigs'
     SQLALCHEMY_TRACK_MODIFICATIONS = True
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
